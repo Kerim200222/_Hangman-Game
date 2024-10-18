3b3186ec3f2a922ed33d2de56d097ebad6b17dcb
ABDULKARIM | عبدالكريم <115738542+Kerim200222@users.noreply.github.com>
2024-10-18 18:19:49 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace AdamAsmacaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 75);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Image = global::AdamAsmacaa.Properties.Resources.AdamAsmaca_0;
+            this.button1.Location = new System.Drawing.Point(24, 430);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Guess";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(199, 449);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(63, 28);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Traditional Arabic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(104, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 49);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "The hidden word,";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(80, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(352, 59);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Adam Asmaca";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Image = global::AdamAsmacaa.Properties.Resources.AdamAsmaca_0;
+            this.button2.Location = new System.Drawing.Point(288, 432);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackgroundImage = global::AdamAsmacaa.Properties.Resources.AdamAsmaca_0;
+            this.pictureBox1.Location = new System.Drawing.Point(76, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 197);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(459, 501);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Adam Asmaca";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+
+namespace AdamAsmacaa
+{
+    public partial class Form1 : Form
+    {
+        #region Constructors
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Methods
+
+        #region Variables
+        // Kelime listesi, oyun sırasında rastgele bir kelimenin seçilmesi için kullanılır.
+        List<string> lstWords = new List<string>()
+        {
+            "adana","konya", "gaziantep", "bursa","bursa", "antalya", "izmir","ankara",
+            "mersin","elazig","kilis","diyarbakir"
+        };
+        int incorrectGuess;  // Yanlış tahmin sayısını takip eder.
+        Random random;  // Rastgele kelime seçimi için kullanılır.
+        string selectedWord;  // Seçilen kelimeyi depolar.
+        char[] displayword;  // Kullanıcıya gösterilecek olan kelimenin tahmin edilmiş harflerini depolar.
+        #endregion
+
+        // Adam asmaca resmi güncellenir, yanlış tahmin sayısına göre resimler değişir.
+        private void UpdateAdamAsmacaImage()
+        {
+            incorrectGuess++;  // Yanlış tahmin sayısını artırır.
+            switch (incorrectGuess)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.AdamAsmaca_1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.AdamAsmaca_2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.AdamAsmaca_3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.AdamAsmaca_4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.AdamAsmaca_5;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.AdamAsmaca_6;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources.AdamAsmaca_7;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources.AdamAsmaca_8;
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources.AdamAsmaca_9;
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources.AdamAsmaca_10;
+                    playSoundGameOver(); // Oyun bitti sesi çalar.
+                    MessageBox.Show("You,ve lost! The word was " + selectedWord); // Oyuncuya kaybettiğini ve kelimeyi gösterir.
+                    Application.Restart(); // Oyun yeniden başlatılır.
+                    break;
+            }
+        }
+
+        // Hata sesi çalmak için kullanılır.
+        public void playSoundError()
+        {
+            using (var audioFile = new AudioFileReader("C:\\Users\\abooa\\source\\repos\\AdamAsmacaa\\Resources\\error.mp3"))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(100); // Ses çalma işlemi bitene kadar beklenir.
+                }
+            }
+        }
+
+        // Oyun bittiğinde çalan ses.
+        public void playSoundGameOver()
+        {
+            using (var audioFile = new AudioFileReader("C:\\Users\\abooa\\source\\repos\\AdamAsmacaa\\Resources\\gameOver.mp3"))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        // Doğru tahmin yapıldığında çalan ses.
+        public void playSoundDing()
+        {
+            using (var audioFile = new AudioFileReader("C:\\Users\\abooa\\source\\repos\\AdamAsmacaa\\Resources\\ding.mp3"))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        // Rastgele bir kelime seçer ve oyun başlangıç durumunu sıfırlar.
+        public void RandomWord()
+        {
+            incorrectGuess = 0; // Yanlış tahmin sayısını sıfırlar.
+            random = new Random();
+            selectedWord = lstWords[random.Next(lstWords.Count)];// Liste içerisinden rastgele bir kelime seçer.
+            displayword = new string('_', selectedWord.Length).ToCharArray();// Kelimenin her harfini _ ile gösterir.
+            string formattedDisplayWord = string.Join(" ", displayword);
+            label1.Text = formattedDisplayWord;// Kelimenin her harfini _ ile gösterir.
+        }
+        #endregion
+
+        #region UI Elements
+        // Form yüklendiğinde rastgele kelime seçilir.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RandomWord();
+        }
+
+        // Tahmin edilen harfin kontrolünü yapan buton işlemi.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char guess = textBox1.Text.ToLower()[0];// Girilen harfi alır ve küçük harfe çevirir.
+            bool correctGuess = false; // Doğru tahmin yapılıp yapılmadığını takip eder.
+            for (int i = 0; i < selectedWord.Length; i++)
+            {
+                if (selectedWord[i] == guess)// Girilen harf kelimede varsa
+                {
+                    displayword[i] = guess;// Harfi doğru yere koyar.
+                    correctGuess = true;
+                }
+            }
+
+            label1.Text = string.Join(" ", displayword);// Güncellenen kelimeyi ekranda gösterir.
+            if (!correctGuess) // Eğer tahmin yanlışsa
+            {
+                UpdateAdamAsmacaImage();// Adam asmaca resmini günceller.
+                playSoundError(); // Hata sesi çalar.
+                textBox1.Clear();// TextBox'ı temizler.
+            }
+            else// Eğer tahmin doğruysa
+            {
+                playSoundDing();// Doğru tahmin sesi çalar.
+                textBox1.Clear();// TextBox'ı temizler.
+            }
+
+            if (!label1.Text.Contains('_'))// Eğer tüm harfler doğru tahmin edilmişse
+            {
+                MessageBox.Show("Congratulations! You,ve won");// Kazanma mesajı gösterir.
+                Application.Restart();// Oyun yeniden başlar.
+            }
+        }
+
+        // Yeni bir oyun başlatmak için kullanılan buton
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RandomWord();// Yeni bir kelime seçer ve oyunu sıfırlar.
+        }
+        #endregion
+    }
+}
